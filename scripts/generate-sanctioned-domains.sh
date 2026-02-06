185ab6fefc2cb385bff9c95514118ef2bf7bad35
Sadjad Dehghani <42276140+FDeghy@users.noreply.github.com>
2026-02-06 17:28:00 +0330

    Fix GitHub API endpoint for sanctioned domains download
    Updated the GitHub API endpoint for downloading sanctioned domains and commented out the second API call.

--- scripts/generate-sanctioned-domains.sh
+++ scripts/generate-sanctioned-domains.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Download the lists using github api
-gh api https://api.github.com/repos/MrDevAnony/DynX-AntiBan-Domains/contents/DynX-AntiBan-list.lst -H "Accept: application/vnd.github.raw" > sanctioned-raw.txt
-gh api https://api.github.com/repos/filteryab/ir-sanctioned-domain/contents/data/ir-sanctioned-domain -H "Accept: application/vnd.github.raw" >> sanctioned-raw.txt
+gh api https://api.github.com/repos/FDeghy/DynX-AntiBan-Domains/contents/DynX-AntiBan-list.lst -H "Accept: application/vnd.github.raw" > sanctioned-raw.txt
+#gh api https://api.github.com/repos/filteryab/ir-sanctioned-domain/contents/data/ir-sanctioned-domain -H "Accept: application/vnd.github.raw" >> sanctioned-raw.txt
 
 # Clean, deduplicate and sort the lists
 cat sanctioned-raw.txt | sed -e 's/^\(|\|\*\|\.\|\-\|0\.0\.0\.0\|127\.0\.0\.1\)*//g' -e 's/\^.*$//g' -e '/!\|?\|@\|#\|\*\|_\|\\\|\/\|\[\|]\|\[\|\([0-9]\{1,3\}\.\)\{3\}[0-9]\{1,3\}/d' -e '/\.$/d' -e '/^\s*$/d' | awk '{$1=$1};1' | perl -CSDA -pe 's/[\x{200C}\x{200D}]//g' | dos2unix | idn2 --no-alabelroundtrip --no-tr46 | LC_ALL=C sort -u > sanctioned-temp.txt
